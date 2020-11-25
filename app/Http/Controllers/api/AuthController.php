e026153184e7adb817ee99a24dd5fba326c08b3c
sjahn <32211334+asj214@users.noreply.github.com>
2020-11-25 15:37:09 +0900

    sanctum 1.1

---
+++ app/Http/Controllers/api/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+
+class AuthController extends Controller
+{
+    //
+    public function register(Request $request){
+
+        $validator = Validator::make($request->all(), [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:4', 'confirmed'],
+        ]);
+
+        if ($validator->fails()) {
+            //
+            return response()->json(['error' => '..']);
+        }
+
+        $user = new User;
+        $user->email = $request->email;
+        $user->name = $request->name;
+        $user->password = bcrypt($request->password);
+        $user->password = Hash::make($request->password);
+        $user->save();
+
+    }
+
+}
