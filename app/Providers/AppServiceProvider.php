0ffee68ef97753126f3628acd54f360d289cf9d5
sjahn <32211334+asj214@users.noreply.github.com>
2020-12-07 10:16:23 +0900

    luatic 전용 설정...

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use DB;
 use Log;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,6 +27,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Schema::defaultStringLength(191);
+
         if (env('APP_DEBUG', false)) {
             DB::listen(function($query){
 
